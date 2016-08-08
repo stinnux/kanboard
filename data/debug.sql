0f75938b7f2af675a24e2fe9d0d2960c63d4adf2
Thomas Stinner <thomas.stinner@sap.com>
2016-08-08 17:10:49 +0200

    wip

--- data/debug.sql
+++ data/debug.sql
@@ -1,1 +1,13 @@
-SELECT COUNT(*) FROM "subtask_time_tracking" LEFT JOIN "subtasks" ON "subtasks"."id"="subtask_time_tracking"."subtask_id" LEFT JOIN "tasks" ON "tasks"."id"="subtasks"."task_id" LEFT JOIN "users" ON "users"."id"="subtask_time_tracking"."user_id" LEFT JOIN "projects" ON "projects"."id"="tasks"."project_id" WHERE subtask_time_tracking.is_billable = ? AND subtask_time_tracking.start >= ? AND subtask_time_tracking.start <= ? ORDER BY tasks.project_id ASC, subtasks.task_id ASC, subtask_time_tracking.subtask_id ASC, subtask_time_tracking.start ASC
+SELECT COUNT(*)
+FROM "subtask_time_tracking"
+LEFT JOIN "subtasks" ON "subtasks"."id"="subtask_time_tracking"."subtask_id"
+LEFT JOIN "tasks" ON "tasks"."id"="subtasks"."task_id"
+LEFT JOIN "users" ON "users"."id"="subtask_time_tracking"."user_id"
+LEFT JOIN "projects" ON "projects"."id"="tasks"."project_id"
+WHERE SUBTASK_TIME_TRACKING.IS_BILLABLE = 1
+    AND SUBTASK_TIME_TRACKING.START >= 1467331200
+    AND SUBTASK_TIME_TRACKING.START <= 1470614400
+ORDER BY TASKS.PROJECT_ID ASC,
+         SUBTASKS.TASK_ID ASC,
+         SUBTASK_TIME_TRACKING.SUBTASK_ID ASC,
+         SUBTASK_TIME_TRACKING.START ASC
