2613e5a3aceaaaed30d9102c98ebb97acd66f3ef
stinnux <me@stinnux.de>
2016-08-17 09:17:56 +0200

    Spent vs. billable

--- app/Controller/ProjectAnalyticsController.php
+++ app/Controller/ProjectAnalyticsController.php
@@ -52,6 +52,42 @@ class ProjectAnalyticsController extends BaseController
     )));
 
     }
+
+    public function spentvsbillable()
+    {
+
+      $values = $this->request->getValues();
+
+      if (empty($values)) {
+        $values['from'] = $this->request->getStringParam('from', date('Y-m-d', strtotime('first day of last month')));
+        $values['to'] = $this->request->getStringParam('to', date('Y-m-d', strtotime('last day of last month')));
+      }
+
+      $groupinator = $this->groupinator
+        ->setUrl("ProjectAnalyticsController","spentvsbillable")
+        ->setQuery($this->subtaskTimeTrackingModel->getTimeSpentVsTimeBillableQuery(
+            $this->dateParser->removeTimeFromTimestamp($this->dateParser->getTimestamp($values['from'])),
+            $this->dateParser->removeTimeFromTimestamp($this->dateParser->getTimestamp($values['to']))))
+        ->addAggregate('case when '.SubtaskTimeTrackingModel::TABLE.'.is_billable = 1 then '.SubtaskTimeTrackingModel::TABLE.'.time_spent else 0', Groupinator::SUM)
+        ->addAggregate('case when '.SubtaskTimeTrackingModel::TABLE.'.is_billable = 0 then '.SubtaskTimeTrackingModel::TABLE.'.time_spent else 0', Groupinator::SUM)
+        ->addGroup(SubtaskTimeTrackingModel::TABLE.'.is_billable', 'Billable', array('header' => Groupinator::ALWAYS, 'footer' => Groupinator::ALWAYS))
+        ->addGroup(SubtaskTimeTrackingModel::TABLE.'.subtask_id', 'Subtask', array('header' => Groupinator::NEVER, 'footer' => Groupinator::ALWAYS))
+        ->addGroup(SubtaskModel::TABLE.'.task_id', 'Task', array('header' => Groupinator::NEVER, 'footer' => Groupinator::LAST))
+        ->addGroup(TaskModel::TABLE.'.project_id', 'Project', array('header' => Groupinator::FIRST, 'footer' => Groupinator::LAST))
+        ->setDetails("project_analytics/details")
+        ->setMax(20)
+        ->calculate();
+
+        $this->response->html($this->helper->layout->projectAnalytics('project_analytics/spentvsbillable', array(
+            'title' => t("Time Spent vs. Time Billable"),
+            'function' => 'spentvsbillable',
+            'values' => $values,
+            'paginator' => $groupinator,
+            'date_format' => $this->configModel->get('application_date_format'),
+            'date_formats' => $this->dateParser->getAvailableFormats($this->dateParser->getDateFormats()),
+        )));
+
+    }
 }
 
  ?>
---
+++ app/Template/project_analytics/spentvsbillable.php
@@ -0,0 +1,65 @@
+
+<?= $this->render('project_analytics/daterange',
+      array('values' => $values,
+            'function' => $function,
+            'date_format' => $date_format,
+            'date_formats' => $date_formats,
+      )
+); ?>
+
+
+<?php if ($paginator->isEmpty()): ?>
+    <p class="alert"><?= t('No Subtask Time Tracking entries found.') ?></p>
+<?php elseif (!$paginator->isEmpty()): ?>
+    <?php  var_dump($paginator->getCollection()); ?>
+    <table class="table-analytics">
+        <tr>
+            <th class="column-10"><?= t('User') ?></th>
+            <th><?= t('comment') ?></th>
+            <th class="column-20"><?= t('Start') ?></th>
+            <th class="column-10 right"><?= t('Time billable') ?></th>
+        </tr>
+        <?php foreach ($paginator->getCollection() as $record): ?>
+          <?php if ($record['groupType'] == "header"): ?>
+            <?php if ($record['groupName'] == "Project"): ?>
+              <?= $this->render('project_analytics/project_header', array(
+                      'values' => $record['values'],
+                      'groupValues' => $record['groupValues'])
+                    ); ?>
+            <?php endif ?>
+          <?php endif ?>
+          <?php if ($record['groupType'] == "footer"): ?>
+            <?php if ($record['groupName'] == "Task"): ?>
+              <?= $this->render('project_analytics/task_footer',
+                array('values' => $record["values"],
+                      'groupValues' => $record["groupValues"]
+                    )
+                ); ?>
+            <?php endif ?>
+            <?php if ($record['groupName'] == 'Subtask'): ?>
+              <?= $this->render('project_analytics/subtask_footer', array(
+                  'values' => $record['values'],
+                  'groupValues' => $record['groupValues'])
+                ); ?>
+            <?php endif ?>
+            <?php if ($record['groupName'] == 'Project'): ?>
+              <?= $this->render('project_analytics/project_footer', array(
+                  'values' => $record['values'],
+                  'groupValues' => $record['groupValues'])
+                ); ?>
+            <?php endif ?>
+          <?php endif ?>
+          <?php if ($record['groupType'] == 'details'): ?>
+            <?php foreach ($record['values'] as $values): ?>
+            <tr>
+                <td><?= $this->url->link($this->text->e($values['user_fullname'] ?: $values['username']), 'UserViewController', 'show', array('user_id' => $values['user_id'])) ?></td>
+                <td><?= $this->text->markdown($values['comment']) ?></td>
+                <td><?= $this->dt->date($values['start']) ?></td>
+                <td class='right'><?= n($values['time_spent']).' '.t('hours') ?></td>
+            </tr>
+          <?php endforeach ?>
+          <?php endif ?>
+        <?php endforeach ?>
+    </table>
+    <?= $paginator ?>
+<?php endif ?>
